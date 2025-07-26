7ec0f3f70dd49e0e31c379ae854167cec03ef623
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-07-26 20:29:17 +0000

    Added Pictures

--- carousel.js
+++ carousel.js
@@ -1,7 +1,7 @@
     const imageUrls = [
-      "https://codehs.com/uploads/7a69e0edd90a208df914b080f5ab27b8?text=Image+1",
-      "https://codehs.com/uploads/7a69e0edd90a208df914b080f5ab27b8?text=Image+2",
-      "https://codehs.com/uploads/7a69e0edd90a208df914b080f5ab27b8?text=Image+3",
+      "https://codehs.com/uploads/e66bb94f3c41d5831d6f5ac30e974804?text=Image+1",
+      "https://codehs.com/uploads/29a33481d460ace59d247da4d910b0d5?text=Image+2",
+      "https://codehs.com/uploads/a9d7dd4ef9b857a0570db3d09e78a0d7?text=Image+3",
       "https://codehs.com/uploads/7a69e0edd90a208df914b080f5ab27b8?text=Image+4",
       "https://codehs.com/uploads/7a69e0edd90a208df914b080f5ab27b8?text=Image+5"
     ];
